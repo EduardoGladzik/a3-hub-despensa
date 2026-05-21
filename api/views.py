67b8d22974f0ca3bcaf0c90c18c51e5759031530
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-05-21 09:21:19 -0300

    feat: views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Ingredient, Storage, Invoice
+from .serializers import UserSerializer, IngredientSerializer, StorageSerializer, InvoiceSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class IngredientViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class StorageViewSet(viewsets.ModelViewSet):
+    queryset = Storage.objects.all()
+    serializer_class = StorageSerializer
+
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
