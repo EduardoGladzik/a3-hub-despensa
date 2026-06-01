40324c6ff2b7a0a25827aa025b2c84973928c018
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-06-01 13:15:04 -0300

    feat: register database models in Django admin.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(StorageIngredient)
+admin.site.register(Invoice)
+admin.site.register(User)
+admin.site.register(Storage)
