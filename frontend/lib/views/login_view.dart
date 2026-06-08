9bea00970e4e219cf40f724a00becef48269b9ae
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-06-08 17:58:46 -0300

    feat: integrate login route to home scrren

--- frontend/lib/views/login_view.dart
+++ frontend/lib/views/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_view.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -29,7 +30,7 @@ class _LoginViewState extends State<LoginView> {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Login realizado com sucesso!')),
       );
-      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeView()));
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeView()));
     }
   }
 
