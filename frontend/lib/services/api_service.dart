a24964e54e6ac31dc15c9a5b965b92fd0b3a5247
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-06-09 21:42:15 -0300

    feat: create ApiService for invoice image upload

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'http://192.168.101.21:8000/api';
 
   Future<bool> uploadInvoice(File imageFile) async {
     var uri = Uri.parse('$baseUrl/invoices/');
@@ -10,27 +10,27 @@ class ApiService {
     try {
       // cria um request
       var request = http.MultipartRequest('POST', uri);
+      
+      // campos obrigatórios injetados manualmente
+      request.fields['user'] = '3';
+      request.fields['destined_storage'] = '2';
+      
       // recebe imagem
       var multipartFile = await http.MultipartFile.fromPath('img_captured', imageFile.path);
       request.files.add(multipartFile);
-
-      print('Enviando imagem...');
 
       // envia o request
       var response = await request.send();
 
       if (response.statusCode == 200 || response.statusCode == 201) {
-        print('Nota processada pelo OCR');
         // JSON de resposta
         //var responseData = await response.stream.byteToString();
         //var data = json.decode(responseData);
         return true;
       } else {
-        print('Erro. Código: ${response.statusCode}');
         return false;
       }
     } catch (e) {
-      print('Erro de comunicação com o backend: $e');
       return false;
     }
   }
